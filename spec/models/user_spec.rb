1f4bc1b85d9919a2c14f05855c7b98a35fbc696d
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-15 01:04:55 +0400

    test: wrap User model specs in validations block

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  subject { build(:user) }
+  describe 'validations' do
+    subject { build(:user) }
 
-  context 'when validating email' do
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-    it { is_expected.to allow_value('email@example.com').for(:email) }
-    it { is_expected.not_to allow_value('invalid_email').for(:email) }
-  end
+    context 'when validating email' do
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+      it { is_expected.to allow_value('email@example.com').for(:email) }
+      it { is_expected.not_to allow_value('invalid_email').for(:email) }
+    end
 
-  context 'when validating password' do
-    it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_confirmation_of(:password) }
-    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    context 'when validating password' do
+      it { is_expected.to validate_presence_of(:password) }
+      it { is_expected.to validate_confirmation_of(:password) }
+      it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    end
   end
 end
