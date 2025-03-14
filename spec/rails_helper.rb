9d319931e5137f6d18137940ec30bf187f0bac97
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-14 22:31:04 +0400

    test: configure cuprite and database_cleaner in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
@@ -18,26 +19,43 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
-  config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-  Capybara.default_driver = :rack_test
-  Capybara.default_max_wait_time = 5
-
-  config.before(:each, :js) do
-    Capybara.current_driver = :selenium_chrome_headless
-  end
-
-  config.after(:each, :js) do
-    Capybara.use_default_driver
-  end
 
   Shoulda::Matchers.configure do |matcher_config|
     matcher_config.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
+  end
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :cuprite
+  end
+
+  Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :system, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
