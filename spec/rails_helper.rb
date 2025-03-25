6a24b79b252ebf09d219291803999d44107e0bd8
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-25 17:39:29 +0400

    test: add login_as helper for system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include SystemHelpers, type: :system
 
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
