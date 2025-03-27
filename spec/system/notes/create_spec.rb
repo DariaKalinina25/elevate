b718f0f5fc0ce3b0396f2886ea18f2d2c1aa4887
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-27 11:38:30 +0400

    test(system): refactor create_spec.rb

--- spec/system/notes/create_spec.rb
+++ spec/system/notes/create_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'Notes create' do
   context 'when the user left the title blank' do
     before { create_note(' ', note.content) }
 
-    it 'redirects to the note page' do
+    it 'redirects to show' do
       expect(page).to have_current_path(note_path(Note.last), ignore_query: true)
     end
 
@@ -32,15 +32,15 @@ RSpec.describe 'Notes create' do
       expect(page).to have_css('.custom-notice', text: 'Note was successfully created.')
     end
 
-    it 'displays the created note title' do
+    it 'displays a title with the creation date' do
       expect(page).to have_css('h4', text: Date.current.strftime('%d.%m.%Y'))
     end
   end
 
   context 'when the user left the content blank' do
-    before { create_note(note.title,  ' ') }
+    before { create_note(note.title, ' ') }
 
-    it 'remains on the creation page' do
+    it 'remains on the page' do
       expect(page).to have_current_path(new_note_path, ignore_query: true)
     end
 
@@ -52,7 +52,7 @@ RSpec.describe 'Notes create' do
   context 'when the user has entered valid data' do
     before { create_note(note.title, note.content) }
 
-    it 'redirects to the note page' do
+    it 'redirects to show' do
       expect(page).to have_current_path(note_path(Note.last), ignore_query: true)
     end
 
@@ -60,19 +60,19 @@ RSpec.describe 'Notes create' do
       expect(page).to have_css('.custom-notice', text: 'Note was successfully created.')
     end
 
-    it 'displays the created note title' do
+    it 'displays the title' do
       expect(page).to have_css('h4', text: note.title)
     end
   end
 
-  context 'when the user wants to cancel the creation of a note' do
+  context 'when the user canceled the creation' do
     before do
       login_as(user)
       visit new_note_path
       click_link_or_button 'Cancel'
     end
 
-    it 'redirects to the notes page' do
+    it 'redirects to index' do
       expect(page).to have_current_path(notes_path, ignore_query: true)
     end
   end
