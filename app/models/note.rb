35c5a87b4ed11c9a03a9130e2ecc4ca106ca3578
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-28 15:54:06 +0400

    feat: add Note model and user association

---
+++ app/models/note.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Model for user-created notes
+class Note < ApplicationRecord
+  belongs_to :user
+  before_validation :set_title_if_blank
+
+  validates :title, length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 700 }
+
+  def set_title_if_blank
+    return if title.present?
+
+    self.title = Date.current.strftime('%d.%m.%Y')
+  end
+end
