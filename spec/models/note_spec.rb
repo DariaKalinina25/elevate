d5a1a9646055382423dd437d661e399bbba47b46
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-03-28 15:46:46 +0400

    test(model): refactoring and adding a new test in the context of the presence of the title

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -18,31 +18,43 @@ RSpec.describe Note do
 
   describe '#set_title_if_blank' do
     let(:user) { create(:user) }
-    let(:note) { create(:note, user: user) }
-    let(:note_blank) { build(:note, title: '', user: user) }
-    let(:note_nil) { build(:note, title: nil, user: user) }
+    let(:note) { create(:note, title: 'My Title', user: user) }
+
+    context 'when the title is present at creation' do
+      it 'does not set title to current date' do
+        expect(note.title).to eq('My Title')
+      end
+    end
+
+    context 'when the title is present on update' do
+      it 'does not set title to current date' do
+        note.update(title: 'My changed title')
+        expect(note.title).to eq('My changed title')
+      end
+    end
 
     context 'when title is blank on creation' do
+      let(:note_blank) { create(:note, title: '', user: user) }
+      let(:note_nil) { create(:note, title: nil, user: user) }
+
       it 'sets title to current date if title is empty' do
-        note_blank.save
-        expect(note_blank.title).to eq(today_str)
+        expect(note_blank.title).to eq(current_date_str)
       end
 
       it 'sets title to current date if title is nil' do
-        note_nil.save
-        expect(note_nil.title).to eq(today_str)
+        expect(note_nil.title).to eq(current_date_str)
       end
     end
 
     context 'when title is blank on update' do
       it 'sets title to current date if title is empty' do
         note.update(title: '')
-        expect(note.title).to eq(today_str)
+        expect(note.title).to eq(current_date_str)
       end
 
       it 'sets title to current date if title is nil' do
         note.update(title: nil)
-        expect(note.title).to eq(today_str)
+        expect(note.title).to eq(current_date_str)
       end
     end
   end
