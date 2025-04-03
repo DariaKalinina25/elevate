d525f39da1d2ede03f8087bca6a583bfc40e009e
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-04-03 12:36:50 +0400

    test(request): redirect to root when note not found

---
+++ spec/requests/rescue_record_not_found_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Rescue from RecordNotFound' do
+  let(:user) { create(:user) }
+
+  context 'when user tries to access a non-existent note' do
+    before do
+      login_as(user)
+      get note_path(-1)
+    end
+
+    it 'redirects to the root path' do
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'shows a not found alert message' do
+      follow_redirect!
+      expect(response.body).to include(t('errors.not_found'))
+    end
+  end
+end
