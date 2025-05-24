9732096bb754a77c9eea845273da665c7d17c37f
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-05-24 22:15:58 +0400

    feat: add TimersController

---
+++ app/controllers/timers_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Controller for user timers
+class TimersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_timer, only: %i[stop destroy]
+  before_action :load_timers, only: %i[index create stop destroy]
+
+  def index
+    @timer = current_user.timers.started.last || build_timer
+  end
+
+  def create
+    @timer = TimerCreator.new(
+      user: current_user,
+      duration_seconds: timer_params[:duration_seconds].to_i,
+      title: timer_params[:title]
+    ).call
+
+    respond_with_success(timers_path) if @timer.persisted?
+  end
+
+  def stop
+    @timer.stop
+    @stopwatch = build_timer
+    respond_with_success(timers_path)
+  end
+
+  def destroy
+    @timer.destroy
+    respond_with_success(timers_path)
+  end
+
+  private
+
+  def set_timer
+    @timer = current_user.timers.find(params[:id])
+  end
+
+  def load_timers
+    @stopwatches = current_user.timers.stopped_recent
+  end
+
+  def build_timer
+    current_user.timers.build
+  end
+
+  def timer_params
+    params.expect(timer: [:title, :duration_seconds])
+  end
+end
