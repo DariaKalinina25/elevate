cc09ac71fdd69966939a6477a8dc5c120e433243
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-05-28 18:38:42 +0400

    test(system): add test covering timers

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -8,4 +8,10 @@ module SystemHelpers
   def have_test(id)
     have_css("[data-testid='#{id}']")
   end
+
+  def fill_input_by_testid(testid, value)
+    page.execute_script(%{
+      document.querySelector('[data-testid="#{testid}"]').value = '#{value}'
+    })
+  end
 end
